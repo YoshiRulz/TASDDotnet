8d34d4eda488ca95592b2decfdbe597dcd9561e6
YoshiRulz <OSSYoshiRulz+git@gmail.com>
2023-12-11 08:40:08 +1000

    bump versions, require SDK 8

--- shell.nix
+++ shell.nix
@@ -1,8 +1,6 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/23.05.tar.gz") {}
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/23.11.tar.gz") {}
 }: pkgs.mkShell {
-	packages = builtins.attrValues {
-		inherit (pkgs) dotnet-sdk_8 kate;
-	};
+	packages = builtins.attrValues { inherit (pkgs) dotnet-sdk_8 kate; };
 	shellHook = ''
 		export DOTNET_ROOT="${pkgs.dotnet-runtime_8}"
 	'';
